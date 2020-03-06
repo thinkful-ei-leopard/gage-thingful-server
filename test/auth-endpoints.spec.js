e6cee7819444e3b20218d2668e29338979693836
gage117 <57643501+gage117@users.noreply.github.com>
2020-03-06 15:52:53 -0600

    Implemented JWT

---
+++ test/auth-endpoints.spec.js
@@ -0,0 +1,89 @@
+const knex = require('knex');
+const jwt = require('jsonwebtoken');
+const app = require('../src/app');
+const helpers = require('./test-helpers');
+
+describe('Auth Endpoints', () => {
+  let db;
+  const { testUsers } = helpers.makeThingsFixtures();
+  const testUser = testUsers[0];
+
+  before('make knex instance', () => {
+    db = knex({
+      client: 'pg',
+      connection: process.env.TEST_DB_URL,
+    })
+    app.set('db', db)
+  })
+
+  after('disconnect from db', () => db.destroy())
+
+  before('cleanup', () => helpers.cleanTables(db))
+
+  afterEach('cleanup', () => helpers.cleanTables(db))
+
+  describe(`POST /api/auth/login`, () => {
+    beforeEach('insert users', () =>
+      helpers.seedUsers(
+        db,
+        testUsers,
+      )
+    )
+
+    const requiredFields = ['user_name', 'password']
+
+    requiredFields.forEach(field => {
+      const loginAttemptBody = {
+        user_name: testUser.user_name,
+        password: testUser.password,
+      }
+
+      it(`responds with 400 required error when '${field}' is missing`, () => {
+        delete loginAttemptBody[field]
+
+        return supertest(app)
+          .post('/api/auth/login')
+          .send(loginAttemptBody)
+          .expect(400, {
+            error: `Missing '${field}' in request body`,
+          })
+      })
+
+      it(`responds 400 'invalid user_name or password' when bad user_name`, () => {
+        const invalidUser = { user_name: 'invalid-user', password: 'existy'}
+        return supertest(app)
+          .post('/api/auth/login')
+          .send(invalidUser)
+          .expect(400, {error: `Incorrect user_name or password`})
+      })
+
+      it(`responds 400 'invalid user_name or password' when bad user_name`, () => {
+        const invalidPass = { user_name: testUser.user_name, password: 'invalid'}
+        return supertest(app)
+          .post('/api/auth/login')
+          .send(invalidPass)
+          .expect(400, {error: `Incorrect user_name or password`})
+      })
+
+      it(`responds 200 and JWT auth token using secret when valid credentials`, () => {
+        const validCreds = {
+          user_name: testUser.user_name,
+          password: testUser.password
+        }
+        const expectedToken = jwt.sign(
+          {user_id: testUser.id},
+          process.env.JWT_SECRET,
+          {
+            subject: testUser.user_name,
+            algorithm: 'HS256'
+          }
+        )
+        
+        return supertest(app)
+          .post('/api/auth/login')
+          .send(validCreds)
+          .expect(200, {authToken: expectedToken})
+      })
+    })
+  })
+})
